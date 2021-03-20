8522eadcc3ce54ef35cb07886483ae5c9939b5f4
Marcin Gruchała <margruchala@gmail.com>
2021-03-20 23:50:01 +0100

    Added IHDR chunk basic readr in chunkReader.py

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 import sys 
 import cv2 as cv
+import png 
 
 class Decoder:
     def __init__(self, filePath):
-        self.img = cv.imread(filePath, cv.IMREAD_COLOR)
-        self.imgWidth = 0
-        self.imgHeight = 0
+        self.img = png.Reader(filePath)
+        self.width = None
+        self.height = None
+        self.bitDepth = None
+        self.colorType = None
+        self.compressionMethod = None
+        self.interlaceMethod = None
+
+    def getColorType(self,metadata):
+        colorType = '' 
+        if(metadata['greyscale']):
+            colorType += 'greyscala'
+        else:
+            colorType += 'RGB'
+        if(metadata['alpha']):
+            colorType += ' + alpha sample'
+        return colorType
+
+
+        
         
     def IHDRChunk(self):
-        self.imgHeight, self.imgWidth = self.img.shape[:2]
+        data = self.img.read()
+        print(data)
+        self.width = data[0]
+        self.height = data[1]
+        self.bitDepth = data[3]['bitdepth']
+        self.colorType = self.getColorType(data[3])
+        self.interlaceMethod = data[3]['interlace']
+        
 
     def printData(self):
-        cv.imshow('PNG image', self.img)
-        print(self.img.shape)
-        print("Width: ", self.imgWidth)
-        print("Height: ", self.imgHeight)
-    pass
+        print(f'IHDR Chunk:\n   Width: {self.width}\n   Height: {self.height}\n   Bit Depth: {self.bitDepth}\n   Color type: {self.colorType}')
+        print(f'   Interlace: {self.interlaceMethod}')
 
 
 
 def main():
-    decoder = Decoder(sys.argv[1])
-    decoder.IHDRChunk()
-    decoder.printData()
-    cv.waitKey(0)
+    png = Decoder(sys.argv[1])
+    png.IHDRChunk()
+    # png.printData()
+    print(png.img.chunk())
+
+
 
 if __name__ == '__main__':
     main()
