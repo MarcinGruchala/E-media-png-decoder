3a8fef74c2338dd6c01a9d756462778cb8686407
Marcin Gruchała <margruchala@gmail.com>
2021-03-20 16:57:22 +0100

    Added width and  height reader

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import sys 
 import cv2 as cv
 
+class Decoder:
+    def __init__(self, filePath):
+        self.img = cv.imread(filePath, cv.IMREAD_COLOR)
+        self.imgWidth = 0
+        self.imgHeight = 0
+        
+    def IHDRChunk(self):
+        self.imgHeight, self.imgWidth = self.img.shape[:2]
+
+    def printData(self):
+        cv.imshow('PNG image', self.img)
+        print(self.img.shape)
+        print("Width: ", self.imgWidth)
+        print("Height: ", self.imgHeight)
+    pass
+
+
+
 def main():
-    img = cv.imread(sys.argv[1])
-    cv.imshow('PNG image', img)
+    decoder = Decoder(sys.argv[1])
+    decoder.IHDRChunk()
+    decoder.printData()
     cv.waitKey(0)
 
 if __name__ == '__main__':
