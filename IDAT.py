64cf21c367226aca3544ce993b81d3f1e71dc565
Marcin Gruchała <margruchala@gmail.com>
2021-06-14 14:16:05 +0200

    idat.py refactor 10/10

--- IDAT.py
+++ IDAT.py
@@ -1,8 +1,10 @@
-import zlib
+'''
+File with IDAT class
+'''
 from chunk import Chunk
 class IDAT(Chunk):
+    '''
+    Class represents IDAT chunk
+    '''
     def __init__(self, chunk):
         super().__init__(chunk.length, chunk.type,chunk.data,chunk.crc)
-
-
-
