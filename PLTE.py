e168697c9def21ddae006146ac4cee9014ea3f10
Marcin Gruchała <margruchala@gmail.com>
2021-06-14 15:14:46 +0200

    basic refactor done

--- PLTE.py
+++ PLTE.py
@@ -1,13 +1,22 @@
+'''
+File with PLTE class.
+'''
 from chunk import Chunk
 class PLTE(Chunk):
-    def __init__(self,length,chunkType,data,crc):
-        super().__init__(length,chunkType,data,crc)
-        self.palette = self.getPalette()
+    '''
+    Class represents PLTE chunk
+    '''
+    def __init__(self,length,chunk_type,data,crc):
+        super().__init__(length,chunk_type,data,crc)
+        self.palette = self.get_palette()
 
-    def getPalette(self):
+    def get_palette(self):
+        '''
+        Method return colour palette
+        '''
         palette = []
-        for bytesIndex in range(0,len(self.data),3):
-            pixel = self.data[bytesIndex:bytesIndex+3]
-            pixelPalette = (pixel[0],pixel[1],pixel[2])
-            palette.append(pixelPalette)
+        for bytes_index in range(0,len(self.data),3):
+            pixel = self.data[bytes_index:bytes_index+3]
+            pixel_palette = (pixel[0],pixel[1],pixel[2])
+            palette.append(pixel_palette)
         return palette
